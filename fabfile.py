2763884eaddcdc2c6d3907a1a07703940b1002c5
Alexandre Leray <alexandre@alexandreleray.com>
2013-10-10 14:51:37 +0200

    A "fabfile" to automate deployment; but as it is it doesn't seem to work

--- fabfile.py
+++ fabfile.py
@@ -3,6 +3,22 @@ from fabric.api import run, local, put, cd, sudo, env, prefix
 from fabric.contrib.console import confirm
 
 
+def deploy():
+    """deploys to previously setup environment"""
+    env.hosts = ['admin@95.142.166.47']
+    env.path = '/srv/data_phenix/fr.stdin.phenix/app'
+    path_activate = '/srv/data_phenix/fr.stdin.phenix/venv/bin/activate'
+    path_wsgi = '/srv/data_phenix/fr.stdin.phenix/app/phenix/wsgi.py'
+
+    with cd(env.path):
+        run('git pull origin master')
+
+        with prefix('source %s' % path_activate):
+            run('python manage.py collectstatic --noinput')
+
+    run('touch %s' % path_wsgi)
+
+
 def download():
     """synchronizes the local db and media files from the remote ones"""
     db_path = '/srv/data_phenix/fr.stdin.phenix/db/phenix.db'
