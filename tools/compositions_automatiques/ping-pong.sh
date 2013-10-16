5d0adba78b6924297599ba148836d9b96625d15f
Alexandre Leray <alexandre@alexandreleray.com>
2013-10-16 15:54:04 +0200

    Working together with Myriam is very helpful to make our head around our new idea: from the collection of recording Sandra and Myriam took, and the accompaning indexation they started to write, we are using some shell scripts to create automatic compositions based on musical patterns such as loops, alternating fragments etc.
    Next plan is to query directly the RDF index to create those
    compositions.

--- tools/compositions_automatiques/ping-pong.sh
+++ tools/compositions_automatiques/ping-pong.sh
@@ -3,8 +3,12 @@
 # the two repo of files
 #PATH_A="/home/aleray/work/osp.work.maisons-phenix.www/tools/compositions_automatiques/MARQUEURS/MA_CLEM_1"
 #PATH_B="/home/aleray/work/osp.work.maisons-phenix.www/tools/compositions_automatiques/MARQUEURS/MA_ALEX_1"
-PATH_A="/home/aleray/work/osp.work.maisons-phenix.www/tools/compositions_automatiques/MARQUEURS/BOUCHE_CLEM"
-PATH_B="/home/aleray/work/osp.work.maisons-phenix.www/tools/compositions_automatiques/MARQUEURS/BOUCHE_CLEM"
+#PATH_A="/home/aleray/work/osp.work.maisons-phenix.www/tools/compositions_automatiques/MARQUEURS/BOUCHE_CLEM"
+#PATH_B="/home/aleray/work/osp.work.maisons-phenix.www/tools/compositions_automatiques/MARQUEURS/BOUCHE_CLEM"
+
+PATH_A="/home/aleray/work/osp.work.maisons-phenix.www/tools/compositions_automatiques/MARQUEURS/ALEX"
+#PATH_B="/home/aleray/work/osp.work.maisons-phenix.www/tools/compositions_automatiques/MARQUEURS/NEW_INDEX_CLEMENT_RESTER_PARTIR"
+PATH_B="/home/aleray/work/osp.work.maisons-phenix.www/tools/compositions_automatiques/MARQUEURS/INDEX_M"
 
 # The number of files to pick
 N=5
@@ -13,7 +17,9 @@ N=5
 FILELIST='/tmp/list.txt'
 
 # We empty that file first
-echo > $FILELIST
+> $FILELIST
+
+ffmpeg -ar 44100 -t 1 -f s16le -acodec pcm_s16le -ac 2 -i /dev/zero -acodec copy /tmp/silence.aiff
 
 #find "${PATH_A}" -iname "*.aiff" -printf "'%p'\n" | shuf -n $N
 
@@ -24,7 +30,9 @@ ARRAY_B=(`find "${PATH_B}" -iname "*.aiff" -printf "file@'%p'\n" | shuf -n $N | 
 for i in {0..4}
 do
    echo ${ARRAY_A[$i]} | tr "@" " " >> "$FILELIST"
+   echo "file '/tmp/silence.aiff'" >> "$FILELIST"
    echo ${ARRAY_B[$i]} | tr "@" " " >> "$FILELIST"
+   echo "file '/tmp/silence.aiff'" >> "$FILELIST"
 done
 
 ffmpeg -y -f concat -i "$FILELIST" -c copy /tmp/output.aiff
